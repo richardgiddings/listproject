e5485bc4e42ee25ff449b1fc4262bb03178b209d
Richard Giddings <rgiddings20@hotmail.com>
2016-04-25 15:26:48 +0100

    Changed deprecated context_instance in edit_view

--- todosite/views.py
+++ todosite/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render, render_to_response
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponseRedirect
 from django.template import RequestContext
@@ -91,9 +91,7 @@ def edit_task(request, task_id=None, template_name='todosite/task.html'):
             form.save()
             return loggedin(request)
 
-    return render_to_response(template_name, {
-        'form': form,
-    }, context_instance=RequestContext(request))
+    return render(request, template_name, context={'form': form})
 
 
 class TaskDelete(DeleteView):
