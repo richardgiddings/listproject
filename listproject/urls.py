322ce8ea0d9d9886c2e10cc405c556d522049c56
Richard Giddings <mail@richardgiddings.co.uk>
2017-04-10 16:00:27 +0100

    Add password reset functionality using django.contrib.auth

--- listproject/urls.py
+++ listproject/urls.py
@@ -16,8 +16,31 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^todosite/', include('todosite.urls')),
+
+    # password reset
+    url(r'^password_reset/$', 
+        auth_views.password_reset, 
+        {'template_name': 'password_reset/password_reset_form.html'}, 
+        name='password_reset'),
+
+    url(r'^password_reset/done/$', 
+        auth_views.password_reset_done, 
+        {'template_name': 'password_reset/password_reset_done.html'}, 
+        name='password_reset_done'),
+    
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        auth_views.password_reset_confirm, 
+        {'template_name': 'password_reset/password_reset_confirm.html'}, 
+        name='password_reset_confirm'),
+    
+    url(r'^reset/done/$', 
+        auth_views.password_reset_complete, 
+        {'template_name': 'password_reset/password_reset_complete.html'}, 
+        name='password_reset_complete'),
 ]
